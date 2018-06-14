3a1e9dd3733c541bc19deb18a90d12517fcc113c
Roman Kotov <waydeveloper@gmail.com>
2018-06-14 14:37:03 +0300

    Initial Commit

---
+++ RKAdapters/RKAdapters.h
@@ -0,0 +1,19 @@
+//
+//  RKAdapters.h
+//  RKAdapters
+//
+//  Created by Roman Kotov on 14/06/2018.
+//  Copyright © 2018 Roman Kotov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RKAdapters.
+FOUNDATION_EXPORT double RKAdaptersVersionNumber;
+
+//! Project version string for RKAdapters.
+FOUNDATION_EXPORT const unsigned char RKAdaptersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RKAdapters/PublicHeader.h>
+
+
